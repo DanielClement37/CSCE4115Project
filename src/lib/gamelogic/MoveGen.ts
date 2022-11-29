e3baaff23736e77012fbf2537a7ebebd861cde60
danielclement37 <daniel.clement37@gmail.com>
2022-11-29 12:05:04 -0600

    Black Pawn Move Gen works

--- src/lib/gamelogic/MoveGen.ts
+++ src/lib/gamelogic/MoveGen.ts
@@ -137,5 +137,60 @@ const GenWhitePawnMoves = (board: Piece[], from: number, enPassantSquare: number
 
 const GenBlackPawnMoves = (board: Piece[], from: number, enPassantSquare: number | null, color: Color): Move[] => {
   let legalMoves: Move[] = new Array(0);
+  let [x, y] = ConvertToXY(from);
+
+  //diagonal kill
+  if (y < 7 && x < 7) {
+    //right attack
+    let diagSquare = board[from + 8 + 1];
+    if (diagSquare.color !== color && (diagSquare.type !== PieceType.EMPTY || enPassantSquare === from + 8 + 1)) {
+      let moveType: MoveType;
+      if (enPassantSquare === from + 8 + 1) {
+        moveType = MoveType.ENPASSANT;
+      } else if (diagSquare.type === PieceType.KING) {
+        moveType = MoveType.CHECK;
+      } else {
+        moveType = MoveType.CAPTURE;
+      }
+      legalMoves.push(CreateMove(color, from, from + 8 + 1, PieceType.PAWN, moveType));
+    }
+  }
+  if (x > 0 && y < 7) {
+    //left attack
+    let diagSquare = board[from + 8 - 1];
+    if (diagSquare.color !== color && (diagSquare.type !== PieceType.EMPTY || enPassantSquare === from + 8 - 1)) {
+      let moveType: MoveType;
+      if (enPassantSquare === from + 8 - 1) {
+        moveType = MoveType.ENPASSANT;
+      } else if (y - 1 === 0) {
+        moveType = MoveType.PROMOTION_CAPTURE;
+      } else {
+        switch (diagSquare.type) {
+          case PieceType.KING:
+            moveType = MoveType.CHECK;
+            break;
+          default:
+            moveType = MoveType.CAPTURE;
+            break;
+        }
+      }
+      legalMoves.push(CreateMove(color, from, from + 8 - 1, PieceType.PAWN, moveType));
+    }
+  }
+
+  //one forward
+  if (y - 1 > 0 && board[from + 8].type === PieceType.EMPTY) {
+    legalMoves.push(CreateMove(color, from, from + 8, PieceType.PAWN, MoveType.QUIET));
+  } else if (y - 1 === 0 && board[from + 8].type === PieceType.EMPTY) {
+    legalMoves.push(CreateMove(color, from, from + 8, PieceType.PAWN, MoveType.PROMOTION));
+  }
+
+  //two forward
+  if (y === 1 && board[from + 16].type === PieceType.EMPTY && board[from + 8].type === PieceType.EMPTY) {
+    let to = from + 16;
+    let enPassantSquare = from + 8;
+    legalMoves.push(CreateMoveTwoSquare(color, from, to, enPassantSquare, PieceType.PAWN, MoveType.PROMOTION));
+  }
+
   return legalMoves;
 };
